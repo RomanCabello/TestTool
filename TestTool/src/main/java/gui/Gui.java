4e2ec521f7de5d4ee8375cd04f54cc3eb45c6310
Cabello. Roman <rcabelloberber@gmail.com>
2019-10-02 10:27:09 -0500

    Added close button
    Now threads won't be kept running and used ports will be freed

--- TestTool/src/main/java/gui/Gui.java
+++ TestTool/src/main/java/gui/Gui.java
@@ -1,6 +1,9 @@
 package gui;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -27,6 +30,15 @@ public class Gui {
 		JButton server = new JButton("Server");
 		server.setBounds(230, 100, 100, 40);
 		server.addActionListener(sra);
+		
+		JButton kill = new JButton("Close");
+		kill.setBounds(130, 250, 100, 40);
+		kill.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				System.exit(0);
+			}
+		});
 		
 		final JSpinner port1 = new JSpinner();
 		port1.setBounds(30, 200, 50, 30);
@@ -63,6 +75,7 @@ public class Gui {
 		
 		f.add(client);	
 		f.add(server);
+		f.add(kill);
 		
 		f.setSize(400, 500);// 400 width and 500 height
 		f.setLayout(null);// using no layout managers
